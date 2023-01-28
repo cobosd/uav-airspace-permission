6ee4849edb0b0c82b22955d0822c44b3b3270b4c
mayanktap <mayanktaparia1992@gmail.com>
2023-01-28 20:46:38 +0530

    Add cognito in amplify and sign in / signup UI.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "uavairspacepermissio0077e7ae": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import { Routes, Route } from "react-router-dom";
 import MapView from "./pages/MapView";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import { Amplify } from 'aws-amplify'
+import awsconfig from './aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(awsconfig);
 
 const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<MapView />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <Routes>
+            <Route path="/" element={<MapView />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </main>
+      )}
+    </Authenticator>
   );
 };
 
