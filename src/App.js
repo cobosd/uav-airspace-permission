b071760c0e401ab7ab270a15509c0fd7d2912803
mayanktap <mayanktaparia1992@gmail.com>
2023-01-28 21:30:03 +0530

    remove old login and signup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import MapView from "./pages/MapView";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
 import { Amplify } from 'aws-amplify'
 import awsconfig from './aws-exports';
 import { Authenticator } from '@aws-amplify/ui-react';
@@ -18,8 +16,6 @@ const App = () => {
           <button onClick={signOut}>Sign out</button>
           <Routes>
             <Route path="/" element={<MapView />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
           </Routes>
         </main>
       )}
