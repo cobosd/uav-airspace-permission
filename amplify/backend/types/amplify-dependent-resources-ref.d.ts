1d0c1ffd3516fe0af75e73aec2095d67ea01f6cd
mayanktap <mayanktaparia1992@gmail.com>
2023-02-03 12:39:14 +0530

    Add S3, Dynamodb and lambda that triggers on S3.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,30 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "S3Triggerd5d77eb7": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dronemediastore": {
+      "BucketName": "string",
+      "Region": "string"
+    },
+    "dronemetadatastore": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
   }
 }
--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { Amplify } from 'aws-amplify'
+import { Storage, Amplify } from 'aws-amplify'
 import awsconfig from './aws-exports';
+import { useState } from "react";
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Button, Heading } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -8,12 +9,36 @@ import "./index.css";
 Amplify.configure(awsconfig);
 
 const App = () => {
+  const [state, setState] = useState([]);
+
+  function onUpload(e) {
+    const file = e.target.files[0];
+    console.log(file);
+
+    Storage.put(file.name, file, {
+      contentType: 'image/png'
+    }).then((result) => {
+      setState({file: URL.createObjectURL(file)});
+      console.log(result);
+    })
+
+  }
+
   return (
     <Authenticator className="uav-airspace">
       {({ signOut, user }) => (
         <main>
           <Heading variation="primary">Hello {user.username}</Heading>
           <Button onClick={signOut}>Sign out</Button>
+          <div>
+            <div>
+              <p>Please select an image to upload</p>
+              <input type="file" onChange={onUpload} />
+            </div>
+            <div>
+              <img alt="" src={state.file} />
+            </div>
+          </div>
           {/* <Routes>
             <Route path="/" element={<MapView />} />
           </Routes> */}
